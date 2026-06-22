9c8af25f0f4c2364525fdfa9451b1deab23a9394
rohan.g <rohan.g@rohangs-MacBook-Pro.local>
2026-06-22 15:45:20 +0530

    blog app and blog model

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ STATIC_ROOT = BASE_DIR/'static'
 STATICFILES_DIRS = [
     'blog_main/static'
 ]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
---
+++ blogs/admin.py
@@ -0,0 +1,15 @@
+from turtle import title
+from unicodedata import category
+
+from django.contrib import admin
+from .models import Category,Blog
+
+class blogadmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('title',)}
+    list_display = ('title' ,'category' , 'author' , 'is_featured' ,'status')
+    search_fields = ( 'id','title', 'category__category_name' , 'status')
+    list_editable = ('is_featured',)
+ 
+
+admin.site.register(Category)
+admin.site.register(Blog,blogadmin)
